0a7282d1bedf52400d797f059a2a6a4af4555029
jesus-banuelos-gomez <98487423+jesus-banuelos-gomez@users.noreply.github.com>
2022-07-04 20:12:28 -0600

    Add files via upload

---
+++ PictourPW/pictourJs/index.js
@@ -0,0 +1,34 @@
+
+function mostrar_datos(){
+	var dato_1 = document.getElementById("name").value;
+	var dato_2 = document.getElementById("email").value
+	
+	var r = dato_1 +" "+ dato_2 ;
+
+	alert(r);
+	console.log(dato_1);
+	console.log(dato_2);
+	console.log("----------------------------");
+}
+
+function nuevo(){
+
+	var dato_1 = document.getElementById("Correo").value;
+	var dato_2 = document.getElementById("password").value;
+
+	var r2 = dato_2 + " " +dato_1;
+	alert(r2);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
